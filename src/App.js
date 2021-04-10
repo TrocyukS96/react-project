c423affba00e1438a5dc161cb72213620a2e0ae4
Falloutdevil <Falloutdevil@ya.ru>
2021-04-10 23:07:03 +0300

    add card details

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import './App.css';
 import Btn from './components/btn/Btn';
 import Footer from './components/footer/Footer';
@@ -6,18 +5,27 @@ import Footer from './components/footer/Footer';
 
 import Header from './header/Header';
 import Stock from "./components/stock/Stock";
+import CardDetails from "./components/cardDetails/CardDetails";
 
 function App() {
-  return (
-    <div className="App">
-      <div className="conteiner">
-        <Header />
-        <Stock />
-        <Btn />
-        <Footer />
-      </div>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="conteiner">
+                <Header />
+                <Stock />
+                <Btn />
+                <div className="CardDetailsContainer">
+                    <CardDetails text="Квадроциклы" />
+                    <CardDetails text="Гидроциклы" />
+                    <CardDetails text="Катера" />
+                    <CardDetails text="Снегоходы" />
+                    <CardDetails text="Вездеходы" />
+                    <CardDetails text="Двигатели" />
+                </div>
+                <Footer />
+            </div>
+        </div>
+    );
 }
 
 export default App;
