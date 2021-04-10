b86ed8082ab0832b5bb20c3d1b421a35a9325a75
Vitalik9135eb2 <76886298+Vitalik9135eb2@users.noreply.github.com>
2021-04-10 19:36:38 +0300

    footer img

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,4 +1,10 @@
 
+
+
+import Inst from "./../../assets/image/social-icons/instagram.svg";
+import Vk from "./../../assets/image/social-icons/vk .svg";
+import Fb from "./../../assets/image/social-icons/facebook .svg";
+import Yt from "./../../assets/image/social-icons/youtube .svg";
 import './Footer.css';
 
 function Footer() {
@@ -6,27 +12,29 @@ function Footer() {
     <div className="footer">
 
           <div>
-            <p> Подпишитесь на нашу рассылку 
+
+            <p> 
+                Подпишитесь на нашу рассылку 
                 и узнавайте о акция быстрее
             </p>
             
-            <input type="text" placeholder="Введите ваш email"/>
+            <input  type="text" placeholder="Введите ваш email"/>
 
-            <button>Отправить</button>
+            <button >Отправить</button>
           </div>
 
-          <div>
+        <div>
           <h4>Информация</h4>
-          <ul>
-            <li><a href="#">О компании</a></li>
+          <ul >
             <li><a href="#">Контакты</a></li>
+            <li><a href="#">О компании</a></li>
             <li><a href="#">Акции</a></li>
             <li><a href="#">Магазины</a></li>
           </ul>
           <a href="#">Договор оферты</a>
         </div>
 
-     <div>
+      <div>
        <h4>Интернет-магазин</h4>
        <ul>
          <li><a href="#">Доставка и самов</a></li>
@@ -36,13 +44,13 @@ function Footer() {
        </ul>
        <a href="#">Политика обработки персональных данных</a>
 
-     </div>
+      </div>
 
      <div>
-       {/* <a><img src="" alt="текст"></a>
-       <a><img src="" alt="текст"></a>
-       <a><img src="" alt="текст"></a>
-       <a><img src="" alt="текст"></a> */}
+       <a href="#"><img src={Inst} alt="текст"/></a>
+       <a href="#"><img src={Vk} alt="текст"/></a>
+       <a href="#"><img src={Fb} alt="текст"/></a>
+       <a href="#"><img src={Yt} alt="текст"/></a>
      </div>
 
     </div>
