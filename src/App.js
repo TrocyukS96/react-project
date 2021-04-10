adff562d83aa4f63f3fc14ce372db262f68b45d6
TrocyukS96 <trotzuk.stanislav@gmail.com>
2021-04-10 13:05:06 +0300

    component Nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
 import Btn from './components/btn/Btn';
+import Nav from './components/nav/Nav';
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <Btn/>
     </div>
   );
