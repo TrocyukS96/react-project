0ec323bd16230fe53ff44119634c32b66b70cf2f
Falloutdevil <Falloutdevil@ya.ru>
2021-04-11 17:18:09 +0300

    add card catalog

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import './App.css';
-import Btn from './components/btn/Btn';
 import Footer from './components/footer/Footer';
 
 
 import Header from './header/Header';
-import Stock from "./components/stock/Stock";
 import BlokSearh from './components/BlokSearh/BlokSerah';
 
 import CardDetails from "./components/cardDetails/CardDetails";
 import CardBaner from './components/cardBaner/CardBaner';
 import Banner from './header/banner/Banner';
 import BlokFilter from './components/BlokFilter/BlokFilter';
+import CardCatalog from "./components/cardCatalog/CardCatalog";
 
 function App() {
     return (
         <div className="App">
             <div className="conteiner">
                 <Header />
-                <Banner/>
-                
-                <BlokSearh/>
+                <Banner />
+
+                <BlokSearh />
                 <div className="CardDetailsContainer">
                     <CardDetails text="Квадроциклы" />
                     <CardDetails text="Гидроциклы" />
@@ -28,18 +27,30 @@ function App() {
                     <CardDetails text="Вездеходы" />
                     <CardDetails text="Двигатели" />
                 </div>
-                <CardBaner/>
+                <CardBaner />
 
                 <Footer />
-
-
-
 
 
                 <Header />
-                
-                <BlokFilter/>
+
+                <BlokFilter />
+                <div className="CardCatalogContainer">
+                    <CardCatalog text="Гидроцикл BRP SeaDoo GTI 130hp SE Black\Mango" text2="1 049 500 ₽"/>
+                    <CardCatalog text="Гидроцикл BRP SeaDoo GTI 155hp SE Long Blue Metallic" text2="1 100 475 ₽"/>
+                    <CardCatalog text="Гидроцикл BRP SeaDoo GTR 230hp X California green " text2="1 323 700 ₽"/>
+                    <CardCatalog text="Гидроцикл BRP SeaDoo GTR 230hp STD Black / Gulfstream" text2="нет в наличии"/>
+                    <CardCatalog text="Гидроцикл BRP SeaDoo GTX 300hp LTD Liquid Metal" text2="1 543 000 ₽"/>
+                    <CardCatalog text="Гидроцикл BRP SeaDoo Spark 60hp 2 up" text2="732 345 ₽"/>
+                    <CardCatalog text="Гидроцикл BRP SeaDoo Spark GTS 90hp Rental" text2="857 666 ₽"/>
+                    <CardCatalog text="Гидроцикл BRP SeaDoo WAKE 230hp PRO Teal blue" text2="1 229 711 ₽"/>
+                    <CardCatalog text="Гидроцикл Spark 2-UP 900 Ho Ace Chili Pepper" text2="587 440 ₽"/>
+                    <CardCatalog text="Гидроцикл Spark 2-UP 900 Ho Ace Pineapple " text2="587 440 ₽"/>
+                    <CardCatalog text="Гидроцикл BRP Sea-doo Spark 2-UP 900 Ace Vanilla " text2="нет в наличии"/>
+                    <CardCatalog text="Гидроцикл Spark 3-UP 900 HO Ace IBR Blueberry" text2="нет в наличии"/>
+                </div>
                 <Footer />
+
             </div>
         </div>
     );
---
+++ src/components/cardCatalog/CardCatalog.js
@@ -0,0 +1,23 @@
+import React from "react";
+import './CardCatalog.css';
+import cardCatalogImg from "./../../assets/image/cardCatalog/cardCatalogImg.svg";
+import favoritesImg from "./../../assets/image/cardCatalog/favoritesImg.svg";
+import saleImg from "./../../assets/image/cardCatalog/saleImg.svg";
+import cartImg from "./../../assets/image/cardCatalog/cartImg.svg";
+
+function CardCatalog(props) {
+    return (
+        <div className="cardCatalog">
+            <img src={saleImg} alt="sale" />
+           <button><img src={favoritesImg} alt="favorites " /></button>
+                <img src={cardCatalogImg} alt="item" />
+                <h2>{props.text}</h2>
+                <span >{props.text2}</span>
+                {/*<span >нет в наличии</span>*/}
+                {/*<a href="#">Сообщить о поступлении</a>*/}
+            <img src={cartImg} alt="cart" />
+        </div>
+    );
+}
+
+export default CardCatalog
