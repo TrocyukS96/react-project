a54b68ba7d102f4bad673d3b5ab60f498f4471e0
Falloutdevil <Falloutdevil@ya.ru>
2021-04-25 17:38:17 +0300

    add img in CardCatalog

--- src/pages/catalog/cardCatalog/CardCatalog.js
+++ src/pages/catalog/cardCatalog/CardCatalog.js
@@ -1,22 +1,22 @@
 import React from "react";
 import s from './CardCatalog.module.scss';
-import cardCatalogImg from "./../../../assets/image/cardCatalog/cardCatalogImg.svg";
 import favoritesImg from "./../../../assets/image/cardCatalog/favoritesImg.svg";
 import saleImg from "./../../../assets/image/cardCatalog/saleImg.svg";
 import cartImg from "./../../../assets/image/cardCatalog/cartImg.svg";
 import {NavLink} from "react-router-dom";
 
 
+
 function CardCatalog(props) {
+
     return (
         <div className={s.cardCatalog}>
             <img src={saleImg} alt="sale" />
             <button className={s.buttonFavorit}><img src={favoritesImg} alt="favorites " /></button>
             <div className={s.cardCatalogInner}>
-                <img src={cardCatalogImg} alt="item" />
-                <h3 className={s.titleCardCatalog}>{props.text}</h3>
-                <span className={s.costCardCatalog}>{props.text2}</span>
-                {/*<span >нет в наличии</span>*/}
+                <img src={props.img} alt="item" />
+                <h3 className={s.titleCardCatalog}>{props.title}</h3>
+                <span className={s.costCardCatalog}>{props.cost}</span>
                 {/*<a href="#">Сообщить о поступлении</a>*/}
                 <button className={s.buttonCart}><img src={cartImg} alt="cart" /></button>
                 <button className={s.showProduct}><NavLink to='/productCard'>посмотреть товар</NavLink></button>
